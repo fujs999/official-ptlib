d424866f944de3e345be6b9f2bf492a225ef1a26
Robert Jongbloed <robert.jongbloed@blackboard.com>
2019-04-26 11:28:10 +0100

    WEBRTC-3432: Fixed subtle problem with reading whole WebSocket messages, and added PWebSocket::ReadText() to read a text/string based message.

--- src/ptclib/httpsrvr.cxx
+++ src/ptclib/httpsrvr.cxx
@@ -1226,7 +1226,7 @@ PBoolean PWebSocket::Read(void * buf, PINDEX len)
   if (len > (PINDEX)m_remainingPayload)
     len = (PINDEX)m_remainingPayload;
 
-  if (!PIndirectChannel::ReadBlock(buf, len))
+  if (!ReadBlock(buf, len))
     goto badRead;
 
   if (m_currentMask >= 0) {
@@ -1265,7 +1265,7 @@ badRead:
 
 bool PWebSocket::ReadMessage(PBYTEArray & msg)
 {
-  if (!PAssert(m_remainingPayload == 0, "Cannot call ReadMessage whan have partial frames unread."))
+  if (!PAssert(m_remainingPayload == 0, "Cannot call ReadMessage when have partial frames unread."))
     return false;
 
   PINDEX totalSize = 0;
@@ -1279,6 +1279,23 @@ bool PWebSocket::ReadMessage(PBYTEArray & msg)
   msg.SetSize(totalSize);
 
   return true;
+}
+
+
+bool PWebSocket::ReadText(PString & msg)
+{
+  PBYTEArray data;
+  if (!ReadMessage(data))
+    return false;
+
+  msg = PString(data);
+  return true;
+}
+
+
+bool PWebSocket::IsMessageComplete() const
+{
+  return !m_fragmentedRead && m_remainingPayload == 0;
 }
 
 
@@ -1406,10 +1423,11 @@ bool PWebSocket::ReadHeader(OpCodes  & opCode,
   opCode = (OpCodes)(header1 & 0xf);
 
   PTimeInterval oldTimeout = GetReadTimeout();
+  SetReadTimeout(1000);
   bool ok = false;
 
   BYTE header2;
-  if (!ReadBlock(&header2, 1))
+  if (!Read(&header2, 1))
     goto badHeader;
 
   switch (header2 & 0x7f) {
