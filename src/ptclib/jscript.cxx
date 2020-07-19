c4663bfe5ebaa59b2c417d246dc1a68735c36afc
Robert Jongbloed <robertj@voxlucida.com.au>
2020-07-19 15:56:05 +0100

    Fixed comment

--- src/ptclib/jscript.cxx
+++ src/ptclib/jscript.cxx
@@ -1,29 +1,28 @@
 /*
-
-* jscript.cxx
-*
-* Interface library for JavaScript interpreter
-*
-* Portable Tools Library
-*
-* Copyright (C) 2012 by Post Increment
-*
-* The contents of this file are subject to the Mozilla Public License
-* Version 1.0 (the "License"); you may not use this file except in
-* compliance with the License. You may obtain a copy of the License at
-* http://www.mozilla.org/MPL/
-*
-* Software distributed under the License is distributed on an "AS IS"
-* basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-* the License for the specific language governing rights and limitations
-* under the License.
-*
-* The Original Code is Portable Tools Library.
-*
-* The Initial Developer of the Original Code is Post Increment
-*
-* Contributor(s): Craig Southeren
-*/
+ * jscript.cxx
+ *
+ * Interface library for JavaScript interpreter
+ *
+ * Portable Tools Library
+ *
+ * Copyright (C) 2012 by Post Increment
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Portable Tools Library.
+ *
+ * The Initial Developer of the Original Code is Post Increment
+ *
+ * Contributor(s): Craig Southeren
+ */
 
 #ifdef __GNUC__
 #pragma implementation "jscript.h"
