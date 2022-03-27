781c90c3190b765fbbf02b1c5b06ff2265b803ce
Robert Jongbloed <robertj@voxlucida.com.au>
2022-03-27 13:44:35 +0000

    Update version number for beta v2.18.8

--- version.h
+++ version.h
@@ -36,8 +36,8 @@
 
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 18
-#define BUILD_TYPE ReleaseCode
-#define PATCH_VERSION 7
+#define BUILD_TYPE BetaCode
+#define PATCH_VERSION 8
 #define OEM_VERSION 0
 
 
