87e6a4578b3a684d9b0275f92e948d3688061130
Robert Jongbloed <robert.jongbloed@classedu.com>
2023-02-14 12:06:25 +0000

    Allow for DTLS v1.2 only enforcement

--- src/ptclib/pssl.cxx
+++ src/ptclib/pssl.cxx
@@ -2157,11 +2157,23 @@ void PSSLContext::Construct(const void * sessionId, PINDEX idSize)
   }
 
 #if (OPENSSL_VERSION_NUMBER >= 0x10100000L)
-  if (m_method == DTLSv1_2_v1_0) {
+  if (m_method >= DTLSv1) {
     PTRACE(5, "Changing security level to zero, was " << SSL_CTX_get_security_level(m_context));
     SSL_CTX_set_security_level(m_context, 0);
-    SSL_CTX_set_min_proto_version(m_context, DTLS1_VERSION);
-    SSL_CTX_set_max_proto_version(m_context, DTLS1_2_VERSION);
+    
+    switch (m_method) {
+      case DTLSv1_2 :
+        SSL_CTX_set_min_proto_version(m_context, DTLS1_2_VERSION);
+        SSL_CTX_set_max_proto_version(m_context, DTLS1_2_VERSION);
+        break;
+      case DTLSv1:
+        SSL_CTX_set_min_proto_version(m_context, DTLS1_VERSION);
+        SSL_CTX_set_max_proto_version(m_context, DTLS1_VERSION);
+        break;
+      default:
+        SSL_CTX_set_min_proto_version(m_context, DTLS1_VERSION);
+        SSL_CTX_set_max_proto_version(m_context, DTLS1_2_VERSION);
+    }
   }
 #endif
 
